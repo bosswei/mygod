13cfe9b522983c40296087b1241166900f7f8061
bosswei <absolutely1000@163.com>
2018-11-26 17:26:12 +0800

    Initial Commit

---
+++ mygod/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mygod
+//
+//  Created by wei on 2018/11/26.
+//  Copyright © 2018年 wells. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
